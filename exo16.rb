20ad59d51906827d9752a62352366a8da7a56e8a
crousty24-bit <allen.kch24@gmail.com>
2026-01-13 19:41:34 +0100

    change logic to match assignment

---
+++ exo16.rb
@@ -0,0 +1,14 @@
+puts "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ?"
+user_number = gets.chomp.to_i
+if user_number < 1 || user_number > 25
+  puts "Nombre invalide"
+else
+  puts "Voici la pyramide :"
+  count = 1
+  while count <= user_number
+    spacing = user_number - count
+    line = " " * spacing + "#" * count
+    puts line
+    count += 1
+  end
+end
