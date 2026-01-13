d1a174c4fe206659cbe0cc772945113c206e4b1e
crousty24-bit <allen.kch24@gmail.com>
2026-01-13 19:43:37 +0100

    add if/else bloc condition

--- pyramide.rb
+++ pyramide.rb
@@ -2,10 +2,12 @@ puts "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ?"
 user_number = gets.chomp.to_i
 if user_number < 1 || user_number > 25
   puts "Nombre invalide"
+else
+  puts "Voici la pyramide :"
+  count = user_number
+  while count >= 1 #start with user_number descending to 1
+    puts "#" * count
+    count -= 1 #decrement by 1
+  end
 end
-puts "Voici la pyramide :"
-count = user_number
-while count >= 1 #start with user_number descending to 1
-  puts "#" * count
-  count -= 1 #decrement by 1
-end
+# alternative version of exo_16 : pyramid descending
