76408a593894753ddba082c5f408b08a95a067a1
crousty24-bit <allen.kch24@gmail.com>
2026-01-13 10:29:32 +0100

    add comment to explain code

--- exo_06.rb
+++ exo_06.rb
@@ -1,4 +1,4 @@
 puts "Give me a number"
-user_number = (gets.chomp.to_i) -1 #parenthetis not mandatory but improve readibility
+user_number = (gets.chomp.to_i) -1 #parenthesis not mandatory but improve readibility
 text = "Hello you ! "
 puts text * user_number
