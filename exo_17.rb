350e36bdb64fa0a5dfa180df4a1e184d825c3405
crousty24-bit <allen.kch24@gmail.com>
2026-01-13 19:56:07 +0100

    change count of second loop to avoid repetition of #

--- exo_17.rb
+++ exo_17.rb
@@ -2,24 +2,25 @@ puts "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ?"
 user_number = gets.chomp.to_i
 if user_number < 1 || user_number > 25
   puts "Nombre invalide"
-end
-puts "Voici la pyramide :"
-#descent pyramid
-count = user_number
-while count >= 1
-  number_of_hash = 2 * count - 1
-  spacing = user_number - count
-  line = " " * spacing + "#" * number_of_hash
-  puts line
-  count -= 1
-end
-#ascent pyramid
-count = 1
-while count <= user_number
-  number_of_hash = 2 * count - 1
-  spacing = user_number - count
-  line = " " * spacing + "#" * number_of_hash
-  puts line
-  count += 1
+else
+  puts "Voici la pyramide :"
+  #descent pyramid
+  count = user_number
+  while count >= 1
+    number_of_hash = 2 * count - 1
+    spacing = user_number - count
+    line = " " * spacing + "#" * number_of_hash
+    puts line
+    count -= 1
+  end
+  #ascent pyramid
+  count = 2 #change to 2 to avoid repetition of "#" count=1
+  while count <= user_number
+    number_of_hash = 2 * count - 1
+    spacing = user_number - count
+    line = " " * spacing + "#" * number_of_hash
+    puts line
+    count += 1
+  end
 end
 puts "Ok c'est un sablier mais c'est stylé de ouf"
