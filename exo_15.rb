aa68a34fea3fe75d54fba42a945a895e10c56fcc
crousty24-bit <allen.kch24@gmail.com>
2026-01-13 19:39:59 +0100

    add if/else bloc condition

--- exo_15.rb
+++ exo_15.rb
@@ -2,10 +2,11 @@ puts "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ?"
 user_number = gets.chomp.to_i
 if user_number < 1 || user_number > 25
   puts "Nombre invalide"
-end
-puts "Voici la pyramide :"
-count = 1
-while count <= user_number #start at 1 ascending to user_number
-  puts "#" * count
-  count += 1
+else #condition bloc 
+  puts "Voici la pyramide :"
+  count = 1
+  while count <= user_number #start at 1 ascending to user_number
+    puts "#" * count
+    count += 1
+  end
 end
