de59dd2b5bb53bf23f310f47e46d9ec569fa8692
crousty24-bit <allen.kch24@gmail.com>
2026-01-13 17:13:41 +0100

    improve readibility & puts/p

--- exo_13.rb
+++ exo_13.rb
@@ -1,8 +1,9 @@
-emails = []
+all_emails = []
 50.times do |i|
   #index start from 1 not 0 ; convert to str ; 2 values starting with '0'
   number = (i+1).to_s.rjust(2, '0') #parenthesis for i+1 cause method call priority to +operator
-  "jean.dupont.#{number}@email.fr"
-  emails <<"jean.dupont.#{number}@email.fr"
+  email = "jean.dupont.#{number}@email.fr"
+  puts email
+  all_emails <<email
 end
-puts "#{emails}"
+p all_emails
