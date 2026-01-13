1bb6b1b2981c32dbe1621c59cda253edd9dcaf94
crousty24-bit <allen.kch24@gmail.com>
2026-01-13 10:48:16 +0100

    add while loop countdown to 0

---
+++ exo_08.rb
@@ -0,0 +1,7 @@
+puts "Give me a number"
+user_number = gets.chomp.to_i
+count = user_number #count start with user num
+while count >= 0
+  puts "counter: #{count}"
+  count -=1 #not increment but decrement by 1
+end
