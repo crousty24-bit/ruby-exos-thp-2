6b3cb446e5c99eec92d2f7045bfd2c635b4ce9ab
crousty24-bit <allen.kch24@gmail.com>
2026-01-13 12:13:09 +0100

    add 2 indepedant conditions

--- exo_12.rb
+++ exo_12.rb
@@ -7,6 +7,11 @@ while count <= current_year
     if (count - user_birth) == ((current_year - user_birth) /2) #condition comparison age equal user's half age
       puts "So #{current_year - count} years ago, you were half the age you are today."
     end #need "end" to close if condition inside loop
-    
+    if (count - user_birth) == 0 #add if condition (not elsif) cause we want several special output
+      puts "Duh, you were just born !"
+    end
+    if (current_year - count) == 0
+      puts "Sorry to say it but you're old.."
+    end
   count += 1
 end
