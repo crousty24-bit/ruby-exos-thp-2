a68fab14d60fabe463c2070fc6c83c6d28d4a727
crousty24-bit <allen.kch24@gmail.com>
2026-01-14 12:11:08 +0100

    remove text str in puts

--- exo_08.rb
+++ exo_08.rb
@@ -2,6 +2,6 @@ puts "Give me a number"
 user_number = gets.chomp.to_i
 count = user_number #count start with user num
 while count >= 0
-  puts "counter: #{count}"
+  puts "#{count}"
   count -=1 #not increment but decrement by 1
 end
